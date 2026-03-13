08843389bcffd3d9e8491f3d83d166de84aaecdb
sk1llon <andrey_forsov.gg@mail.ru>
2026-03-13 03:40:01 +0300

    request methods

--- 2/main.py
+++ 2/main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from typing import List
 
 app = Flask(__name__)
 
-@app.route('/list/<path:args>')
-def sum_and_multi(args):
 
-
+@app.route('/list/', methods=['GET'])
+def sum_and_multi():
+    number_list_a: List[int] = request.args.getlist('numA', int)
+    number_list_b: List[int] = request.args.getlist('numB', int)
+    return 'Сумма чисел в массиве A: {sum_a}\n' \
+           'Сумма чисел в массиве B: {sum_b}\n'.format(
+            sum_a=sum(number_list_a),
+            sum_b=sum(number_list_b)
+            )
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=1111)
+
+# http://localhost:1111/list/?numA=5&numA=2&numA=1&numA=6&numB=6&numB=2&numB=1&numB=5
